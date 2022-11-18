c5ba4d52cc2408688960c7552b8a1ce1d2a4c7fc
ibrahim-anas <aibrahim1354@gmail.com>
2022-11-17 22:01:40 -0500

    Changed app to work with 'tasks' rather than 'notes'

--- BooleanBoard.py
+++ BooleanBoard.py
@@ -7,14 +7,14 @@ from flask import render_template
 from flask import request, session
 from flask import redirect, url_for 
 from database import db
-from models import Note as Note
+from models import Task as Task
 from models import User as User
 from models import Comment as Comment
 from forms import RegisterForm, LoginForm, CommentForm
 import bcrypt
  
 app = Flask(__name__)     # create an app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BooleanBoard_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SECRET_KEY'] = 'SE3155'
 
@@ -24,10 +24,6 @@ db.init_app(app)
 # Setup models
 with app.app_context():
     db.create_all()   # run under the app context
-
-# notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-#           2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-#           3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}}
              
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -39,73 +35,73 @@ def index():
         return render_template('index.html', user=session['user'])
     return render_template('index.html')
 
-@app.route('/notes')
-def get_notes():
+@app.route('/tasks')
+def get_tasks():
     if session.get('user'):
-        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
+        my_tasks = db.session.query(Task).filter_by(user_id=session['user_id']).all()
     
-        return render_template('notes.html', notes = my_notes, user = session['user'])
+        return render_template('tasks.html', tasks = my_tasks, user = session['user'])
     else:
         return redirect(url_for('login'))
 
-@app.route('/notes/<note_id>')
-def get_note(note_id):
+@app.route('/tasks/<task_id>')
+def get_task(task_id):
     if session.get('user'):
-        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+        my_task = db.session.query(Task).filter_by(id=task_id, user_id=session['user_id']).one()
         
         form = CommentForm()
 
-        return render_template('note.html', note = my_note, user = session['user'], form = form)
+        return render_template('task.html', task = my_task, user = session['user'], form = form)
     else:
         return redirect(url_for('login'))
 
-@app.route('/notes/new', methods=['GET', 'POST'])
-def new_note():
+@app.route('/tasks/new', methods=['GET', 'POST'])
+def new_task():
     if session.get('user'):
         if request.method == 'POST':
             title = request.form['title']
-            text = request.form['noteText']
+            text = request.form['taskText']
             from datetime import date
             today = date.today()
             today = today.strftime("%m-%d-%Y")
-            new_record = Note(title, text, today, session['user_id'])
+            new_record = Task(title, text, today, session['user_id'])
             db.session.add(new_record)
             db.session.commit()
 
-            return redirect(url_for('get_notes'))
+            return redirect(url_for('get_tasks'))
         else: 
             return render_template('new.html', user = session['user'])
     else:
         return redirect(url_for('login'))
 
-@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
-def update_note(note_id):
+@app.route('/tasks/edit/<task_id>', methods=['GET', 'POST'])
+def update_task(task_id):
     if session.get('user'):
         if request.method == 'POST':
             title = request.form['title']
-            text = request.form['noteText']
-            note = db.session.query(Note).filter_by(id=note_id).one()
-            note.title = title
-            note.text = text
-            db.session.add(note)
+            text = request.form['taskText']
+            task = db.session.query(Task).filter_by(id=task_id).one()
+            task.title = title
+            task.text = text
+            db.session.add(task)
             db.session.commit()
 
-            return redirect(url_for('get_notes'))
+            return redirect(url_for('get_tasks'))
         else:
-            my_note = db.session.query(Note).filter_by(id=note_id).one()
+            my_task = db.session.query(Task).filter_by(id=task_id).one()
 
-            return render_template('new.html', note=my_note, user=session['user'])
+            return render_template('new.html', task=my_task, user=session['user'])
     else:
         return redirect(url_for('login'))
 
-@app.route('/notes/delete/<note_id>', methods = ['POST'])
-def delete_note(note_id):
+@app.route('/tasks/delete/<task_id>', methods = ['POST'])
+def delete_task(task_id):
     if session.get('user'):
-        my_note = db.session.query(Note).filter_by(id=note_id).one()
-        db.session.delete(my_note)
+        my_task = db.session.query(Task).filter_by(id=task_id).one()
+        db.session.delete(my_task)
         db.session.commit()
 
-        return redirect(url_for('get_notes'))
+        return redirect(url_for('get_tasks'))
     else:
         return redirect(url_for('login'))
 
@@ -130,7 +126,7 @@ def register():
         session['user'] = first_name
         session['user_id'] = new_user.id  # access id value from user model of this newly added user
         # show user dashboard view
-        return redirect(url_for('get_notes'))
+        return redirect(url_for('get_tasks'))
 
     # something went wrong - display register view
     return render_template('register.html', form=form)
@@ -148,7 +144,7 @@ def login():
             session['user'] = the_user.first_name
             session['user_id'] = the_user.id
             # render view
-            return redirect(url_for('get_notes'))
+            return redirect(url_for('get_tasks'))
 
         # password check failed
         # set error message to alert user
@@ -166,19 +162,19 @@ def logout():
 
     return redirect(url_for('index'))
 
-@app.route('/notes/<note_id>/comment', methods=['POST'])
-def new_comment(note_id):
+@app.route('/tasks/<task_id>/comment', methods=['POST'])
+def new_comment(task_id):
     if session.get('user'):
         comment_form = CommentForm()
         # validate_on_submit only validates using POST
         if comment_form.validate_on_submit():
             # get comment data
             comment_text = request.form['comment']
-            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            new_record = Comment(comment_text, int(task_id), session['user_id'])
             db.session.add(new_record)
             db.session.commit()
 
-        return redirect(url_for('get_note', note_id=note_id))
+        return redirect(url_for('get_task', task_id=task_id))
 
     else:
         return redirect(url_for('login'))
@@ -188,6 +184,6 @@ app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debu
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
 
-# Note that we are running with "debug=True", so if you make changes and save it
+# Task that we are running with "debug=True", so if you make changes and save it
 # the server will automatically update. This is great for development but is a
 # security risk for production.
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 from database import db
 import datetime
 
-class Note(db.Model):
+class Task(db.Model):
     id = db.Column("id", db.Integer, primary_key = True)
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    comments = db.relationship("Comment", backref="note", cascade="all, delete-orphan", lazy=True)
+    comments = db.relationship("Comment", backref="task", cascade="all, delete-orphan", lazy=True)
 
     def __init__(self, title, text, date, user_id):
         self.title = title
@@ -22,7 +22,7 @@ class User(db.Model):
     email = db.Column("email", db.String(100))
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
-    notes = db.relationship("Note", backref="user", lazy=True)
+    tasks = db.relationship("Task", backref="user", lazy=True)
     comments = db.relationship("Comment", backref="user", lazy=True)
 
     def __init__(self, first_name, last_name, email, password):
@@ -36,11 +36,11 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_posted = db.Column(db.DateTime, nullable=False)
     content = db.Column(db.VARCHAR, nullable=False)
-    note_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey("task.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-    def __init__(self, content, note_id, user_id):
+    def __init__(self, content, task_id, user_id):
         self.date_posted = datetime.date.today()
         self.content = content
-        self.note_id = note_id
+        self.task_id = task_id
         self.user_id = user_id
